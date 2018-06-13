c4ffab031d68439b6434149abc498a687ef0ffb4
MrAlekhin <arty-hero@yandex.ru>
2018-06-13 01:03:28 -0400

    add tabs with icons

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, StyleSheet, FlatList} from 'react-native';
 
-import ListItem from './ListItem';
+import ListItem from './ListItem/ListItem';
 
 const placeList = props =>{
     return<FlatList 
--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+
+import startMainTabs from '../MainTabs/startMainTabs';
 
 class AuthScreen extends Component{
+    loginHandler = () => {
+        startMainTabs();
+    }
+
     render(){
         return(
             <View>
                 <Text>Auth Screen</Text>
+                <Button title="login" onPress={this.loginHandler}/>
             </View>
         );
     }
