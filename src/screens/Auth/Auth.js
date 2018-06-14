15143ed13d720a4bd2f4c047fd48fed96dc02482
MrAlekhin <arty-hero@yandex.ru>
2018-06-14 13:48:35 -0400

    add background

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import {View, Text, Button, TextInput, StyleSheet} from 'react-native';
+import {View, Text, Button, TextInput, StyleSheet, ImageBackground} from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefautInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
-import MainText from '../../components/UI/MainText/MainText'
+import MainText from '../../components/UI/MainText/MainText';
+import backgroundImage from '../../assets/background.jpg';
 
 class AuthScreen extends Component{
     loginHandler = () => {
@@ -13,18 +14,22 @@ class AuthScreen extends Component{
 
     render(){
         return(
-            <View style={styles.container}>
-                <MainText>
-                    <HeadingText style={styles.textHeading}>Signup</HeadingText>
-                </MainText>
-                <Button title="Switch to login" />
-                <View style={styles.inputContainer}>
-                    <DefautInput placeholder="Your E-Mail Address" style={styles.input} />
-                    <DefautInput placeholder="Password" style={styles.input}/>
-                    <DefautInput placeholder="Confirm Password" style={styles.input}/>
-                </View>
-                <Button title="Register" onPress={this.loginHandler}/>
-            </View>
+            
+                <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+                    <View style={styles.container}>
+                    <MainText>
+                        <HeadingText style={styles.textHeading}>Signup</HeadingText>
+                    </MainText>
+                    <Button title="Switch to login" />
+                    <View style={styles.inputContainer}>
+                        <DefautInput placeholder="Your E-Mail Address" style={styles.input} />
+                        <DefautInput placeholder="Password" style={styles.input}/>
+                        <DefautInput placeholder="Confirm Password" style={styles.input}/>
+                    </View>
+                    <Button title="Register" onPress={this.loginHandler} />
+                    </View>
+                </ImageBackground>
+            
         );
     }
 }
@@ -32,8 +37,12 @@ class AuthScreen extends Component{
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: "center",
         alignItems: "center",
-        justifyContent: "center"
+    },
+    backgroundImage: {
+        width: "100%",
+        flex: 1
     },
     inputContainer: {
         width: "80%"
