f854481d55b5513f95b9a8a420958a3ab33a46d7
MrAlekhin <arty-hero@yandex.ru>
2018-06-14 11:52:18 -0400

    add default input ui

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, TextInput, StyleSheet} from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
+import DefautInput from '../../components/UI/DefaultInput/DefaultInput';
 
 class AuthScreen extends Component{
     loginHandler = () => {
@@ -10,12 +11,36 @@ class AuthScreen extends Component{
 
     render(){
         return(
-            <View>
-                <Text>Auth Screen</Text>
-                <Button title="login" onPress={this.loginHandler}/>
+            <View style={styles.container}>
+                <Text>Please Log In</Text>
+                <Button title="Switch to login" />
+                <View style={styles.inputContainer}>
+                    <DefautInput placeholder="Your E-Mail Address"  />
+                    <DefautInput placeholder="Password" />
+                    <DefautInput placeholder="Confirm Password" />
+                </View>
+                <Button title="Signup" onPress={this.loginHandler}/>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    inputContainer: {
+        width: "80%"
+    },
+    input: {
+        width: "100%",
+        borderWidth: 1,
+        borderColor: "#eee",
+        padding: 5,
+        margin: 8
+    }
+})
 
 export default AuthScreen;
