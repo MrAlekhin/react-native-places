4766e2245e7202f9441c477d501fd68fdf3ea014
MrAlekhin <arty-hero@yandex.ru>
2018-06-17 22:22:13 -0400

    add add share place validation

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -16,6 +16,7 @@ import MainText from '../../components/UI/MainText/MainText';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import PickImage from '../../components/PickImage/PickImage';
 import PickLocation from '../../components/PickLoocation/PickLocation';
+import validate from '../../utility/validation';
 
 class SharePlaceScreen extends Component {
 
@@ -24,7 +25,16 @@ class SharePlaceScreen extends Component {
     }
 
     state = {
-        placeName: ""
+        controls: {
+            placeName: {
+                value: "",
+                valid: false,
+                touched: false,
+                validationRules: {
+                    notEmpty: true
+                }
+            }
+        }
     };
 
     constructor(props){
@@ -43,14 +53,24 @@ class SharePlaceScreen extends Component {
     }
 
     placeNameChangeHandler = val => {
-        this.setState({
-            placeName: val 
+        this.setState(prevState =>{
+           return {
+               controls: {
+                   ...prevState.controls,
+                   placeName: {
+                       ...prevState.controls.placeName,
+                       value: val,
+                       valid: validate(val, prevState.controls.placeName.validationRules),
+                       touched: true
+                   }
+               }
+           }; 
         });
     }
 
     placeAddedHandler = () => {
-        if(this.state.placeName.trim() !== ""){
-            this.props.onAddPlace(this.state.placeName);
+        if(this.state.controls.placeName.value.trim() !== ""){
+            this.props.onAddPlace(this.state.controls.placeName.value);
         }
     }
 
@@ -64,11 +84,14 @@ class SharePlaceScreen extends Component {
                     <PickImage />
                     <PickLocation />
                     <PlaceInput 
-                        placeName={this.state.placeName} 
+                        placeData={this.state.controls.placeName} 
                         onChangeText={this.placeNameChangeHandler} 
                     />
                     <View style={styles.button}>
-                        <Button title="Share the Place!" onPress={this.placeAddedHandler}/>
+                        <Button 
+                        title="Share the Place!"
+                        onPress={this.placeAddedHandler}
+                        disabled={!this.state.controls.placeName.valid}/>
                     </View>
                 </View>
             </ScrollView>
--- src/utility/validation.js
+++ src/utility/validation.js
@@ -11,6 +11,9 @@ const validate = (val, rules, connectedValue) =>{
             case 'equalTo':
                 isValid = isValid && equalToValidator(val, connectedValue[rule]);
                 break;
+            case 'notEmpty':
+                isValid = isValid && notEmptyValidator(val);
+                break;
             default:
                 isValid = true;
         }
@@ -28,6 +31,10 @@ const minLengthValidator = (val, minLength) => {
 
 const equalToValidator = (val, checkValue) => {
     return val === checkValue;
+}
+
+const notEmptyValidator = val => {
+    return val.trim() !== "";
 }
 
 export default validate;
