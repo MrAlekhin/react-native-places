35efb95290d1b9ffffcf4507ff796e9957368ebc
MrAlekhin <arty-hero@yandex.ru>
2018-07-15 20:13:18 -0400

    add map view

--- src/components/PickLoocation/PickLocation.js
+++ src/components/PickLoocation/PickLocation.js
@@ -1,36 +1,53 @@
-import React, {Component} from 'react';
-import {View, Image, Button, StyleSheet, Text } from 'react-native';
+import React, { Component } from "react";
+import {
+  View,
+  Image,
+  Button,
+  StyleSheet,
+  Text,
+  Dimensions
+} from "react-native";
+import MapView from "react-native-maps";
 
-class PickLocation extends Component{
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.placeholder}>
-                    <Text>Map</Text>
-                </View>
-                <View style={styles.button}>
-                    <Button title="Locate Me" onPress={()=>alert('Pick Location!')} />
-                </View>
-            </View>
-        );
+class PickLocation extends Component {
+  state = {
+    focusedLocation: {
+      latitude: 37.7900352,
+      longitude: -122.4013726,
+      latitudeDelta: 0.0122,
+      longitudeDelta:
+        (Dimensions.get("window").width / Dimensions.get("window").height) *
+        0.0122
     }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          initialRegion={this.state.focusedLocation}
+          style={styles.map}
+        />
+        <View style={styles.button}>
+          <Button title="Locate Me" onPress={() => alert("Pick Location!")} />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: "100%",
-        alignItems: "center"
-    },
-    placeholder: {
-        borderWidth: 1,
-        borderColor: "black",
-        backgroundColor: "#eee",
-        width: "80%",
-        height: 150
-    },
-    button: {
-        margin: 8
-    }
+  container: {
+    width: "100%",
+    alignItems: "center"
+  },
+  map: {
+    width: "100%",
+    height: 250
+  },
+  button: {
+    margin: 8
+  }
 });
 
 export default PickLocation;
