6146f1b7303c9a89630cffcc987b87c1b6b7f3d9
MrAlekhin <arty-hero@yandex.ru>
2018-07-21 21:34:03 -0400

    add image picker

---
+++ ios/build/Build/Intermediates.noindex/RNImagePicker.build/Debug-iphonesimulator/RNImagePicker.build/Objects-normal/x86_64/ImagePickerManager.d
@@ -0,0 +1,16 @@
+dependencies: \
+  /Users/artem/ReactNative/react-native-basics/node_modules/react-native-image-picker/ios/ImagePickerManager.m \
+  /Users/artem/ReactNative/react-native-basics/node_modules/react-native-image-picker/ios/ImagePickerManager.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTConvert.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTAnimationType.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBorderStyle.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTLog.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTAssert.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTUtils.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTPointerEvents.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTTextDecorationLineType.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/Yoga.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGEnums.h \
+  /Users/artem/ReactNative/react-native-basics/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGMacros.h
