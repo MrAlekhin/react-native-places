b204f202c9f8865a97fa5d31cb4fdec27ba1047e
MrAlekhin <arty-hero@yandex.ru>
2018-07-29 16:28:40 -0400

    start auth

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,28 @@
-import {TRY_AUTH} from './actionTypes';
+import { TRY_AUTH } from './actionTypes';
 
 export const tryAuth = (authData) => {
-    return {
-        type: TRY_AUTH,
-        authData: authData,
-    };
+    return dispatch => {
+        dispatch(authSignup(authData))
+    }
+}
+
+export const authSignup = (authData) => {
+    return dispatch => {
+        fetch("https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyC74zClxpl24iQOzRB3O1QsfRchw4WOgkg", {
+            method: "POST",
+            body: JSON.stringify({
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            })
+        }).catch(err => {
+            console.log(err);
+            alert("Authentication failed, please try again!");
+        })
+            .then(res => res.json())
+            .then(parseRes => {
+                console.log(parseRes);
+
+            })
+    }
 }
