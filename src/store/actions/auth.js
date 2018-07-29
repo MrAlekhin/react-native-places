85af9e014550c6f5dd476274045497ebe05bc77b
MrAlekhin <arty-hero@yandex.ru>
2018-07-29 16:29:00 -0400

    start auth

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -14,7 +14,10 @@ export const authSignup = (authData) => {
                 email: authData.email,
                 password: authData.password,
                 returnSecureToken: true
-            })
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
         }).catch(err => {
             console.log(err);
             alert("Authentication failed, please try again!");
